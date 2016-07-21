19d34196dd43ceedb964b4eacb054aa4090dea63
Mike Sausa <mikejsausa@gmail.com>
2016-07-21 14:01:23 -0600

    Added object for ideas container and create Idea Function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var titleInput = $('.title');
 var bodyInput = $('.body');
 var ideaStore = [];
 var quality = 1;
+var $idealist = $('.ideacontainer ul');
 
 $(document).ready(function() {
     var displayObject = JSON.parse(localStorage.getItem("ideaStore"));
@@ -92,3 +93,59 @@ function commitToLocalStorage() {
         // $('.thumbsUp').on('click', function() {
         //
         // })
+
+        function Idea (name, id, quality, body) {
+            this.name = name;
+            this.id = (id || Date.now());
+            this.quality = (quality || 'swill');
+            this.body = body;
+        }
+
+        var IdeasContainer = {
+          ideas:[],
+
+          store: function(){
+            localStorage.setItem('ideas', JSON.stringify(this.ideas));
+          },
+
+          retrieve: function() {
+            var retrieveIdeas = JSON.parse(localStorage.getItem('ideas'));
+              if (retrieveIdeas)
+                for(var i = 0; i < retrieveIdeas; i++){
+                  var idea = retrieveIdeas[i];
+                  this.ideas.push(new Idea(idea.name, idea.id, idea.quality, idea.body));
+                }
+          },
+
+          add: function(name){
+            this.ideas.push(new Idea(name));
+            this.store();
+          },
+
+        remove: function(id){
+          id = psarseInt(id);
+          this.ideas = this.ideas.filter(function(idea){
+              return idea.id !== id;
+            });
+
+          this.store();
+        },
+
+        render: function(){
+          this.ideas.forEach(function(idea){
+            $idealist.prepend("<li>" +
+                                "<article>" +
+                                   "<input class = 'deleteButton' type = 'image' src = 'images/delete.svg' width = 20 height = 20>" +
+                                   "<p>" + idea.name + "</p>" +
+                                   "<p>"+ idea.body + "</p>" +
+                                   "<input class = 'thumbsup' type = 'image' src='images/upvote.svg' width = 20 height = 20>" +
+                                   "<input class = 'thumbsdown' type = 'image' src='images/downvote.svg' width = 20 height = 20>" +
+                                   "<div class = 'ranking'>ranking: swill</div>" +
+                                 "</article>" +
+                               "</li>");
+          });
+        },
+
+
+
+        };
