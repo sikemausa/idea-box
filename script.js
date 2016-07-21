613cbfdd4f8bca55991b829b4a5cfc31bac59ce5
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-07-21 17:52:21 -0600

    Got rendering and Retrieving from localStorage to work

--- script.js
+++ script.js
@@ -4,148 +4,156 @@ var ideaStore = [];
 var quality = 1;
 var $idealist = $('.ideacontainer ul');
 
-$(document).ready(function() {
-    var displayObject = JSON.parse(localStorage.getItem("ideaStore"));
-    if (displayObject) {
-      ideaStore = displayObject;}
-    loadIdeas();
-});
+// $(document).ready(function() {
+//     var displayObject = JSON.parse(localStorage.getItem("ideaStore"));
+//     if (displayObject) {
+//       ideaStore = displayObject;}
+//     loadIdeas();
+// });
+//
+// function loadIdeas(){
+//   for (var i = 0; i < ideaStore.length; i++) {
+//     var storedIdea = ideaStore[i];
+//
+//     $("ul").append(
+//       "<li>" +
+//       "<article class='template'>" +
+//       "<input class = 'deleteButton' type = 'image' src='images/delete.svg' width = 20 height = 20>" +
+//       "<p>" + storedIdea.title + "</p>" +
+//       "<p>"+ storedIdea.body + "</p>" +
+//       "<input class = 'thumbsUp' type = 'image' src='images/upvote.svg' width = 20 height = 20>" +
+//       "<input class = 'thumbsDown' type = 'image' src='images/downvote.svg' width = 20 height = 20>" +
+//       "<div class = 'ranking'>ranking: swill</div>" +
+//       "</article>" +
+//       "</li>");
+//     }
+//   }
+//
+//
+// function addIdea(){
+//   var title = $('.title').val();
+//   var body = bodyInput.val();
+//   var creativeThought = new Idea();
+//   ideaStore.push(creativeThought);
+//   return $('.ideacontainer ul').prepend("<li>" +
+//                                     "<article class='template'>" +
+//                                        "<input class = 'deleteButton' type = 'image' src='images/delete.svg' width = 20 height = 20>" +
+//                                        "<p>" + title + "</p>" +
+//                                        "<p>"+ body + "</p>" +
+//                                        "<input class = 'thumbsUp' type = 'image' src='images/upvote.svg' width = 20 height = 20>" +
+//                                        "<input class = 'thumbsDown' type = 'image' src='images/downvote.svg' width = 20 height = 20>" +
+//                                        "<div class = 'ranking'>ranking: swill</div>" +
+//                                      "</article>" +
+//                                    "</li>");
+// }
+//
+// function Idea(title, body, id) {
+//   this.title = $('.title').val();;
+//   this.body = $('.body').val();
+//   this.id = Date.now();
+// }
+//
+// $('.save').on('click', function(){
+//   addIdea();
+//   commitToLocalStorage();
+//   clearInputs();
+// });
+//
+// function clearInputs() {
+//   $('.title').val('');
+//   $('.body').val('');
+// }
+//
+// function commitToLocalStorage() {
+//   localStorage.setItem("ideaStore", JSON.stringify(ideaStore)); }
+//
+//   $('.ideacontainer ul').on('click', '.deleteButton', function() {
+//       $(this).parents('.template').remove();
+// });
+//
+//
+//     // for (i=0; i<ideaStore.length;i++){
+//     //   $('.ideacontainer ul').prepend("<li>" +
+//     //                                     "<article>" +
+//     //                                        "<input class = 'deleteButton' type = 'image' src = 'images/delete.svg' width = 20 height = 20>" +
+//     //                                        "<p>" + title + "</p>" +
+//     //                                        "<p>"+ body + "</p>" +
+//     //                                        "<input class = 'thumbsup' type = 'image' src='images/upvote.svg' width = 20 height = 20>" +
+//     //                                        "<input class = 'thumbsdown' type = 'image' src='images/downvote.svg' width = 20 height = 20>" +
+//     //                                        "<div class = 'ranking'>ranking: swill</div>" +
+//     //                                      "</article>" +
+//     //                                    "</li>");
+//     // }
 
-function loadIdeas(){
-  for (var i = 0; i < ideaStore.length; i++) {
-    var storedIdea = ideaStore[i];
 
-    $("ul").append(
-      "<li>" +
-      "<article class='template'>" +
-      "<input class = 'deleteButton' type = 'image' src='images/delete.svg' width = 20 height = 20>" +
-      "<p>" + storedIdea.title + "</p>" +
-      "<p>"+ storedIdea.body + "</p>" +
-      "<input class = 'thumbsUp' type = 'image' src='images/upvote.svg' width = 20 height = 20>" +
-      "<input class = 'thumbsDown' type = 'image' src='images/downvote.svg' width = 20 height = 20>" +
-      "<div class = 'ranking'>ranking: swill</div>" +
-      "</article>" +
-      "</li>");
+
+function Idea (name, body, id, quality) {
+  this.name = name;
+  this.body = body;
+  this.id = (id || Date.now());
+  this.quality = (quality || 'swill');
+}
+
+var IdeasContainer = {
+  ideas:[],
+
+  store: function(){
+    localStorage.setItem('ideas', JSON.stringify(this.ideas));
+  },
+
+  retrieve: function() {
+    var retrieveIdeas = JSON.parse(localStorage.getItem('ideas'));
+    if (retrieveIdeas) {
+      for (var i = 0; i < retrieveIdeas.length; i++) {
+var idea = retrieveIdeas[i];
+this.ideas.push(new Idea(idea.name, idea.body, idea.id, idea.quality));
+      }
     }
-  }
+  },
 
+  add: function(name, body){
+    this.ideas.push(new Idea(name, body));
+    this.store();
+  },
 
-function addIdea(){
-  var title = $('.title').val();
-  var body = bodyInput.val();
-  var creativeThought = new Idea();
-  ideaStore.push(creativeThought);
-  return $('.ideacontainer ul').prepend("<li>" +
-                                    "<article class='template'>" +
-                                       "<input class = 'deleteButton' type = 'image' src='images/delete.svg' width = 20 height = 20>" +
-                                       "<p>" + title + "</p>" +
-                                       "<p>"+ body + "</p>" +
-                                       "<input class = 'thumbsUp' type = 'image' src='images/upvote.svg' width = 20 height = 20>" +
-                                       "<input class = 'thumbsDown' type = 'image' src='images/downvote.svg' width = 20 height = 20>" +
-                                       "<div class = 'ranking'>ranking: swill</div>" +
-                                     "</article>" +
-                                   "</li>");
-}
+  remove: function(id){
+    id = psarseInt(id);
+    this.ideas = this.ideas.filter(function(idea){
+      return idea.id !== id;
+    });
+    this.store();
+    this.render();
+  },
 
-function Idea(title, body, id) {
-  this.title = $('.title').val();;
-  this.body = $('.body').val();
-  this.id = Date.now();
-}
+  render: function() {
+    this.ideas.forEach(function(idea){
+      $idealist.append(`
+<li>
+<article class='template'>
+<input class='deleteButton' type='image' src='images/delete.svg' width='20px' height='20px'>
+<p>${idea.name}</p>
+<p>${idea.body}</p>
+<input class='thumbsUp' type='image' src='images/upvote.svg' width='20px' height='20px'>
+<input class='thumbsDown' type='image' src='images/downvote.svg' width='20' height='20'>
+<div class='ranking'>ranking: swill</div>
+</article>
+</li>
+`);
+      });
+    },
+
+  display: function() {
+    this.idea
+    },
+
+  };
 
 $('.save').on('click', function(){
-  addIdea();
-  commitToLocalStorage();
-  clearInputs();
+  IdeasContainer.add($('.title').val(), $('.body').val());
+
 });
 
-function clearInputs() {
-  $('.title').val('');
-  $('.body').val('');
-}
-
-function commitToLocalStorage() {
-  localStorage.setItem("ideaStore", JSON.stringify(ideaStore)); }
-
-  $('.ideacontainer ul').on('click', '.deleteButton', function() {
-      $(this).parents('.template').remove();
+$('document').ready(function () {
+  IdeasContainer.retrieve();
+  IdeasContainer.render();
 });
-
-
-    // for (i=0; i<ideaStore.length;i++){
-    //   $('.ideacontainer ul').prepend("<li>" +
-    //                                     "<article>" +
-    //                                        "<input class = 'deleteButton' type = 'image' src = 'images/delete.svg' width = 20 height = 20>" +
-    //                                        "<p>" + title + "</p>" +
-    //                                        "<p>"+ body + "</p>" +
-    //                                        "<input class = 'thumbsup' type = 'image' src='images/upvote.svg' width = 20 height = 20>" +
-    //                                        "<input class = 'thumbsdown' type = 'image' src='images/downvote.svg' width = 20 height = 20>" +
-    //                                        "<div class = 'ranking'>ranking: swill</div>" +
-    //                                      "</article>" +
-    //                                    "</li>");
-    // }
-
-
-        // function thumbsUp {
-        //     var
-        // }
-        //
-        // $('.thumbsUp').on('click', function() {
-        //
-        // })
-
-        function Idea (name, id, quality, body) {
-            this.name = name;
-            this.id = (id || Date.now());
-            this.quality = (quality || 'swill');
-            this.body = body;
-        }
-
-        var IdeasContainer = {
-          ideas:[],
-
-          store: function(){
-            localStorage.setItem('ideas', JSON.stringify(this.ideas));
-          },
-
-          retrieve: function() {
-            var retrieveIdeas = JSON.parse(localStorage.getItem('ideas'));
-              if (retrieveIdeas)
-                for(var i = 0; i < retrieveIdeas; i++){
-                  var idea = retrieveIdeas[i];
-                  this.ideas.push(new Idea(idea.name, idea.id, idea.quality, idea.body));
-                }
-          },
-
-          add: function(name){
-            this.ideas.push(new Idea(name));
-            this.store();
-          },
-
-        remove: function(id){
-          id = psarseInt(id);
-          this.ideas = this.ideas.filter(function(idea){
-              return idea.id !== id;
-            });
-
-          this.store();
-        },
-
-        render: function(){
-          this.ideas.forEach(function(idea){
-            $idealist.prepend("<li>" +
-                                "<article>" +
-                                   "<input class = 'deleteButton' type = 'image' src = 'images/delete.svg' width = 20 height = 20>" +
-                                   "<p>" + idea.name + "</p>" +
-                                   "<p>"+ idea.body + "</p>" +
-                                   "<input class = 'thumbsup' type = 'image' src='images/upvote.svg' width = 20 height = 20>" +
-                                   "<input class = 'thumbsdown' type = 'image' src='images/downvote.svg' width = 20 height = 20>" +
-                                   "<div class = 'ranking'>ranking: swill</div>" +
-                                 "</article>" +
-                               "</li>");
-          });
-        },
-
-
-
-        };
