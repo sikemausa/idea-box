a1a436c4c18cc976d0c9e96c0899242ffeea5c2a
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-07-25 08:02:16 -0600

    added media screen and refactored

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ var IdeasRepo = {
     var retrieveIdeas = JSON.parse(localStorage.getItem('ideas'));
     if (retrieveIdeas){
       this._ideas = retrieveIdeas.map(function (idea){
-         return new Idea(idea.title, idea.body, idea.id, idea.quality);
-         });
+        return new Idea(idea.title, idea.body, idea.id, idea.quality);
+      });
     }
     this.render();
   },
@@ -37,14 +37,14 @@ var IdeasRepo = {
     this._ideas.forEach(function(idea){
       $idealist.append(`
         <li id='${idea.id}'>
-          <article class='template'>
-            <button class='delete-button'></button>
-            <h3 class = "titlehtml" contenteditable = "true">${idea.title}</h3>
-            <p class = "bodyhtml" contenteditable = "true">${idea.body}</p>
-            <button class='thumbsUp' type='image'></button>
-            <button class='thumbsDown' type='image'></button>
-            <div class='ranking'>ranking: ${idea.quality}</div>
-            </article>
+          <article class="template">
+            <button class="delete-button"></button>
+            <h3 class="titlehtml" contenteditable="true">${idea.title}</h3>
+            <p class="bodyhtml" contenteditable="true">${idea.body}</p>
+            <button class="thumbsUp" type="image"></button>
+            <button class="thumbsDown" type="image"></button>
+            <div class="ranking">ranking: ${idea.quality}</div>
+          </article>
         </li>
         `);
       });
@@ -64,22 +64,17 @@ var IdeasRepo = {
     });
   },
 
-  //   find the id quality for the instance object
-  //   replace the quality based on what it is using IF statements or FOR loop
-  //   restore the new quality in localStorage
-  //   display new quality in DOM
-
   upVote: function(id) {
     id = parseInt(id);  // finding id for specific instance for that object
     var upgradingQuality = IdeasRepo.findId(id);
     if (upgradingQuality.quality == 'plausible') {
       upgradingQuality.quality = 'genius';
-      } else if (upgradingQuality.quality == 'swill') {
+    } else if (upgradingQuality.quality == 'swill') {
       upgradingQuality.quality = 'plausible';
-      } else {
+    } else {
       upgradingQuality.quality = 'genius';
-      }
-      return this.store();
+    }
+    return this.store();
   },
 
   downVote: function(id) {
@@ -89,10 +84,10 @@ var IdeasRepo = {
       upgradingQuality.quality = 'plausible';
     } else if (upgradingQuality.quality == 'plausible') {
       upgradingQuality.quality = 'swill';
-      } else {
+    } else {
       upgradingQuality.quality = 'swill';
-      }
-      return this.store();
+    }
+    return this.store();
   },
 
   clear: function() {
@@ -100,87 +95,86 @@ var IdeasRepo = {
     $('.body').val('');
     $('.search-idea').val('');
   }
-  };
+};
 
+  $('.save').on('click', function(){
+    IdeasRepo.add($('.title').val(), $('.body').val());
+    IdeasRepo.render();
+    IdeasRepo.clear();
+  });
 
-$('.save').on('click', function(){
-  IdeasRepo.add($('.title').val(), $('.body').val());
-  IdeasRepo.render();
-  IdeasRepo.clear();
-});
+  $('ul').on('click', '.thumbsUp', function() {
+    var id = parseInt(this.closest('li').id);
+    IdeasRepo.upVote(id);
+  });
 
-$('ul').on('click', '.thumbsUp', function() {
-  var id = parseInt(this.closest('li').id);
-  IdeasRepo.upVote(id);
-});
+  $('ul').on('click', '.thumbsDown', function() {
+    var id = parseInt(this.closest('li').id);
+    IdeasRepo.downVote(id);
+  });
 
-$('ul').on('click', '.thumbsDown', function() {
-  var id = parseInt(this.closest('li').id);
-  IdeasRepo.downVote(id);
-});
+  $('ul').on('click', '.delete-button', function() {
+    var id = parseInt(this.closest('li').id);
+    IdeasRepo.remove(id); // We need to traverse to the correct part of the DOM to find the id in the html of this particular idea
+    $(this).parents('.template').remove();
+  });
 
-$('ul').on('click', '.delete-button', function() {
-  var id = parseInt(this.closest('li').id);
-  IdeasRepo.remove(id); // We need to traverse to the correct part of the DOM to find the id in the html of this particular idea
-  $(this).parents('.template').remove();
-});
+  $('document').ready(function () {
+    IdeasRepo.retrieve();
+    IdeasRepo.render();
+  });
 
-$('document').ready(function () {
-  IdeasRepo.retrieve();
-  IdeasRepo.render();
-});
-
-$("ul").on('keydown', '.titlehtml', function(e) {
+  $("ul").on('keydown', '.titlehtml', function(e) {
     if(e.keyCode == 13)
     {
-        e.preventDefault();
-        $(this).focusout();
-    }
-});
-
-$("ul").on('keydown', '.bodyhtml', function(e) {
-    if(e.keyCode == 13)
-    {
-        e.preventDefault();
-        $(this).focusout();
-    }
-});
-
-$("ul").on('focusout', '.titlehtml', function(e) {
-  var id = parseInt(this.closest('li').id);
-  var retrieve = JSON.parse(localStorage.getItem('ideas'));
-  var title = this.textContent;
-  for (var i = 0; i < retrieve.length; i++) {
-    if (retrieve[i].id === id){
-      retrieve[i].title = title;
-    }
-  }
-  localStorage.setItem('ideas', JSON.stringify(retrieve));
-  IdeasRepo.retrieve();
-  this.blur();
-});
-
-$("ul").on('focusout', '.bodyhtml', function(e) {
-  var id = parseInt(this.closest('li').id);
-  var retrieve = JSON.parse(localStorage.getItem('ideas'));
-  var body = this.textContent;
-  for (var i = 0; i < retrieve.length; i++) {
-    if (retrieve[i].id === id){
-      retrieve[i].body = body;
-    }
-  }
-  localStorage.setItem('ideas', JSON.stringify(retrieve));
-  IdeasRepo.retrieve();
-  this.blur();
-});
-
-$('.search-idea').on('keyup', function(event) {
-  var searchContent = $(this).val().toLowerCase();
-  $('li').each(function() {
-    if ($(this).text().search(new RegExp(searchContent, 'i')) < 0) {
-      $(this).hide();
-    } else {
-      $(this).show();
+      e.preventDefault();
+      $(this).focusout();
     }
   });
-});
+
+  $("ul").on('keydown', '.bodyhtml', function(e) {
+    if(e.keyCode == 13)
+    {
+      e.preventDefault();
+      $(this).focusout();
+    }
+  });
+
+  $("ul").on('focusout', '.titlehtml', function(e) {
+    var id = parseInt(this.closest('li').id);
+    var retrieve = JSON.parse(localStorage.getItem('ideas'));
+    var title = this.textContent;
+    for (var i = 0; i < retrieve.length; i++) {
+      if (retrieve[i].id === id){
+        retrieve[i].title = title;
+      }
+    }
+    localStorage.setItem('ideas', JSON.stringify(retrieve));
+    IdeasRepo.retrieve();
+    this.blur();
+  });
+
+  $("ul").on('focusout', '.bodyhtml', function(e) {
+    var id = parseInt(this.closest('li').id);
+    var retrieve = JSON.parse(localStorage.getItem('ideas'));
+    var body = this.textContent;
+    for (var i = 0; i < retrieve.length; i++) {
+      if (retrieve[i].id === id){
+        retrieve[i].body = body;
+      }
+    }
+    localStorage.setItem('ideas', JSON.stringify(retrieve));
+    IdeasRepo.retrieve();
+    this.blur();
+  });
+
+  $('.search-idea').on('keyup', function(event) {
+    var searchContent = $(this).val().toLowerCase();
+    $('li').each(function() {
+      if ($(this).text().search(new RegExp(searchContent, 'i')) < 0) {
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
+    });
+  });
